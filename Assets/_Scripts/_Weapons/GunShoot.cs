1da22428c829e8754035c782de517c49ab91641d
killahowais <jamadowais@gmail.com>
2025-01-16 18:00:10 +0100

    ammo score added and animation tweaked

--- Assets/_Scripts/_Weapons/GunShoot.cs
+++ Assets/_Scripts/_Weapons/GunShoot.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 public interface IDamageable
 {
     public void Damage(float damageAmount) 
@@ -13,6 +14,7 @@ public class GunShoot : MonoBehaviour
 {
     // Scripts 
     [SerializeField] private Animations Animations;
+    [SerializeField] private _PlayerMovement playerMovement;
 
     // Data
     public string GunName;
@@ -27,20 +29,19 @@ public class GunShoot : MonoBehaviour
     private bool _isShooting = false;
     private bool _isReloading = false;
     private bool _isShootingCoroutineRunning = false;
+
+    [SerializeField] private TextMeshProUGUI _currentAmmoText;
     [SerializeField] Transform _shootPos;
     [SerializeField] SpriteRenderer _gunSprite;
 
 
 
-    //private void Update()
-    //{
-    //    if (_isShootingCoroutineRunning)
-    //    {
-    //        Animations.IdleAnimation();
-    //    }
-    //}
+    private void Update()
+    {
+        _currentAmmoText.text = "Current Ammo: " + CurrentAmmo.ToString();
+    }
 
-    // update for the button to call this coroutine
+    // function for the button to call this coroutine
     public void StartShooting()
     {
         _isShooting = true;
@@ -60,10 +61,12 @@ public class GunShoot : MonoBehaviour
     IEnumerator Shoot() 
     {
         _isShootingCoroutineRunning = true;
+
         while (_isShooting)
         {
             if (CurrentAmmo != 0 && !_isReloading)
             {
+
                 RaycastHit2D hit = Physics2D.Raycast(_shootPos.position, _shootPos.right, 15f);
                 Debug.DrawRay(_shootPos.position, _shootPos.right * 15, Color.magenta);
                 CurrentAmmo--;
@@ -89,7 +92,6 @@ public class GunShoot : MonoBehaviour
         yield return null;
     }
         
-
     // reload data
     IEnumerator ReloadGun()
     {
